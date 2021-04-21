406d60ca3fe90464ed51de83bfdb15a47bbe0a0a
CarmiSmith <75535889+CarmiSmith@users.noreply.github.com>
2021-04-20 21:44:41 -0400

    still working on charts

--- static/barchart1.js
+++ static/barchart1.js
@@ -14,20 +14,9 @@ function buildMetadata(selection) {
     var tbody = d3.select("#data-table").append("tbody")
     var tr = tbody.append("tr")
     tr.append('td').text(chosenTeam.team)
-    tr.append('td').text(chosenTeam.Price_per_Ounce)
+    tr.append('td').text(chosenTeam.Average_Price_per_Ounce)
     tr.append('td').text(chosenTeam.year)
     tr.append('td').text(chosenTeam.wins)
-
-
-
-    
-      //Data.forEach((d) => {
-      // Filter the data to get the sample's metadata
-     // var filtData = d.team;
-      //console.log(filtData)
-      //var sample = filtData.filter(item => item.id.toString() == selection)[0];
-      //})
-      // Specify the location of the metadata and update it
       
   });
 }
@@ -40,22 +29,16 @@ function buildCharts(selection) {
     
       Data.forEach((d) => {
         var filtData = d.team;
-        var sampleDict = filtData.filter(item => item.team == selection)[0];
-        var sampleValues = sampleDict.team; 
-        var idValues = sampleDict.Average_Price_per_Ounce;
-        var barLabels = idValues.slice(0, 10).reverse();
-        var newLabels = [];
-        barLabels.forEach((label) => {
-            newLabels.push("Team " + label);
-        });
-        var hovertext = sampleDict.team; 
+        var price = d.Average_Price_per_Ounce
+        var wins = d.year
+        var hovertext = filtData; 
 
               // Create bar chart in correct location
       var barTrace = {
         type: "bar",
-        y: newLabels,
-        x: sampleValues.slice(0, 10).reverse(),
-        text: hovertext.slice(0, 10).reverse(),
+        y: wins,
+        x: price,
+        text: hovertext,
         orientation: 'h'
     };
 
@@ -70,24 +53,14 @@ function buildCharts(selection) {
 
       Plotly.newPlot("bar", barData, barLayout);
       })
-      // Filter the data to get the sample's OTU data
       
-
-      
-
-
-
-
       // Create bubble chart in correct location
       var bubbleTrace = {
-          x: idValues,
-          y: sampleValues,
+          x: price,
+          y: wins,
           text: hovertext,
           mode: "markers",
-          marker: {
-              color: idValues,
-              size: sampleValues
-          }
+
       };
 
       var bubbleData = [bubbleTrace];
@@ -112,32 +85,17 @@ function init() {
   // Read json data
   d3.json("json/teambeers.json").then((Data) => {
       Data.forEach((d) => {
-        //console.log(d.team)
            // Filter data to get sample names
         var filtData = d.team;
          // Add dropdown option for each sample
         var dropdownMenu = d3.select("#table-selector-dropdown");
         dropdownMenu.append("option").property("value", filtData).text(filtData)
+      });
+
         //buildMetadata(filtData[0]);
 
-        //buildCharts(filtData[0]);
-        //console.log(filtData)
-              // Add names to the drop down
-        //filtData.forEach((team) => {
-        //console.log(team)
-          //dropdownMenu.append("option").property("value", team).text(team);
-      //})
-      });
-      
-      
-   
-
-     
-
-
-      // Use first sample to build metadata and initial plots
+        buildCharts(filtData[0]);
     
-
   });
 }
 
